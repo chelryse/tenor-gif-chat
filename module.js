eabf1e05cbd3111f79bb1ccb76abcee49eb43155
chelryse <104970133+chelryse@users.noreply.github.com>
2025-05-15 07:45:07 -0300

    Add files via upload

---
+++ module.js
@@ -0,0 +1,10 @@
+{
+  "name": "tenor-gif-chat",
+  "title": "Tenor GIF Chat Button",
+  "description": "Adiciona um botão lateral que permite pesquisar e enviar GIFs da Tenor para o chat.",
+  "version": "1.0",
+  "author": "Caio e ChatGPT",
+  "minimumCoreVersion": "12",
+  "compatibleCoreVersion": "12",
+  "scripts": ["scripts/main.js"]
+}
